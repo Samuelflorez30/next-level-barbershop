f393c720b2af6414476802397be30aa2aa7aa12e
Samuel David Florez Ramirez <davidflorezramirez1602@gmail.com>
2026-09-04 07:58:30 -0500

    fix: agregar propiedad site en la configuracion para habilitar sitemap xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://barbernextlevel.com', // <-- Esta línea genera la magia del sitemap
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [sitemap()]
 });
